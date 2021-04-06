2ed319334f99fc5fe252237a1dc01d9217678475
KyleWalkerley <walkerleykyle@gmail.com>
2021-04-06 11:48:57 +0200

    first commit

---
+++ code.py
@@ -0,0 +1,52 @@
+#x = 15
+#price = 9.99
+
+#discount = 0.2
+
+#result = price * (1-discount)
+
+#print(result)
+
+
+#name = "Rolf"
+
+#print(name)
+
+#print(name * 2)
+
+#a = 25
+
+#b = a 
+
+#print(a)
+#print(b)
+
+#b = 17
+
+#print(a)
+#print(b)
+
+
+#name = "Bob"
+#greeting = f"Hello, {name}"
+
+#print(greeting)
+
+#name = "Bob"
+#greeting = "Hello,{}"
+#with_name = greeting.format(name)
+#with_name_two = greeting.format("Rolf")
+
+#print(with_name)
+#print(with_name_two)
+
+#size_input = input("How big is your house (in square feet):")
+#square_feet = int(size_input)
+#square_meters = square_feet /10.8
+#print(square_meters)
+
+
+user_age = input("Enter your age:")
+years = int(user_age)
+months = years * 12
+print(f"Your age, {years}, is equal to {months} months.")
