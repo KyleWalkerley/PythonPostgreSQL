6630fb87d87ecc5f78ce3526baff7224abe2deb5
KyleWalkerley <78598168+KyleWalkerley@users.noreply.github.com>
2021-05-17 22:34:00 +0200

    Add files via upload

--- Chapter3/app.py
+++ Chapter3/app.py
@@ -1,3 +1,5 @@
+from abc import create_table, add_entry, get_entries
+
 menu = """ Please select one of the folowing options:
 1) Add new entry for today.
 2) View enteries.
@@ -7,14 +9,28 @@ Your selection:"""
 
 Welcome = "Welcome to the programing diary!"
 
+def prompt_new_entry():
+    entry_content = input("What have you learned today? ")
+    entry_date = input("Enter the date:")
+
+    add_entry(entry_content, entry_date) 
+
+def view_entries(entries):
+    for entry in entries:
+            print(f"{entry['date']}\n{entry['content']}\n\n")
+
 print(Welcome)
+
+create_table()
 
 user_input = input(menu)
 while (user_input := input(menu)) != "3":
     if user_input == "1":
-        print("Adding...")
+        prompt_new_entry()
+
     elif user_input == "2":
-        print("Viewing...")
+        view_entries(get_entries())
+
     else:
         print("Invalid option, please try again!")
 
