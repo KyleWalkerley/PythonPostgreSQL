7fc5e6d9fd4661c120634554be59f817a80cbf32
KyleWalkerley <78598168+KyleWalkerley@users.noreply.github.com>
2021-05-24 22:21:21 +0200

    Add files via upload

--- PythonPostgreSQL/Chapter4/app.py
+++ PythonPostgreSQL/Chapter4/app.py
@@ -8,7 +8,8 @@ menu = """Please select one of the following options:
 3) View all movies
 4) Watch a movie
 5) View watched movies.
-6) Exit.
+6) Add user to the app.
+7) Exit.
 
 Your selection: """
 welcome = "Welcome to the watchlist app!"
@@ -28,10 +29,10 @@ def prompt_add_movie():
 
 def print_movie_list(heading, movies):
     print(f"-- {heading} movies --")
-    for movie in movies():
-        movie_date = datetime.datetime.fromtimestamp(movie[1])
+    for _id, title, release_date in movies():
+        movie_date = datetime.datetime.fromtimestamp(release_date)
         human_date = movie_date.strftime("%b %d %Y")
-        print(f"{movie[0]} (on {human_date})")
+        print(f"{_id}: {title} (on {human_date})")
     print("---- \n")
 
 
@@ -45,12 +46,15 @@ def print_watched_movie_list(username, movies):
 
 def prompt_watch_movie():
     username = input("Username: ")
-    movie_title = input("Enter movie title you've watched: ")
-    test.watch_movie(movie_title)
+    movie_id = input("Movie ID: ")
+    test.watch_movie(username, movie_id)
+
+def prompt_add_user():
+    username = input("Username: ")
+    test.add_user(username)
 
 
-
-while (user_input := input(menu)) != "6":
+while (user_input := input(menu)) != "7":
     if user_input == "1":
         prompt_add_movie()
     elif user_input == "2":
@@ -65,5 +69,7 @@ while (user_input := input(menu)) != "6":
         username = input("Username: ")
         movies = test.get_watched_movies(username)
         print_watched_movie_list(username, movies)
+    elif user_input == "6":
+        prompt_add_user()
     else:
         print("Invalid input, please try again!")
