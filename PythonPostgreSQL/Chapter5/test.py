8ecc335bc5c6f76db9af38b477412b105d5a9281
KyleWalkerley <78598168+KyleWalkerley@users.noreply.github.com>
2021-05-25 21:27:35 +0200

    Add files via upload

--- PythonPostgreSQL/Chapter5/test.py
+++ PythonPostgreSQL/Chapter5/test.py
@@ -4,7 +4,7 @@ import psycopg2
 
 from dotenv import load_dotenv
 
-load_dotenv
+load_dotenv()
 
 connection = psycopg2.connect(os.environ["TEST_URL"])
 
@@ -25,60 +25,63 @@ CREATE_WATCHED_TABLE = """CREATE TABLE IF NOT EXISTS watched (
     FOREIGN KEY(movie_id) REFERENCES movies(id)
 );"""
 
-INSERT_MOVIES = "INSERT INTO movies (title, release_timestamp) VALUES (?, ?);"
-INSERT_USER = "INSERT INTO users (username) VALUES (?)"
-DELETE_MOVIE = "DELETE FROM movies WHERE title = ?;"
+INSERT_MOVIES = "INSERT INTO movies (title, release_timestamp) VALUES (%s, %s);"
+INSERT_USER = "INSERT INTO users (username) VALUES (%s)"
 SELECT_ALL_MOVIES = "SELECT * FROM movies;"
-SELECT_UPCOMING_MOVIES = "SELECT * FROM movies WHERE release_timestamp > ?;"
+SELECT_UPCOMING_MOVIES = "SELECT * FROM movies WHERE release_timestamp > %s;"
 SELECT_WATCHED_MOVIES = """SELECT movies.* FROM movies 
 JOIN watched ON movie.id = watched.movie_id
 JOIN users ON users.username = watched.user_username
-WHERE users.username = ?;"""
-INSERT_WATCHED_MOVIE = "INSERT INTO watched (user_username, movie_id) VALUES (?, ?)"
-SET_MOVIE_WATCHED = "UPDATE movies SET watched = 1 WHERE title =?;"
-SEARCH_MOVIES = "SELECT * FROM movies WHERE title LIKE ?;"
-CREATE_RELEASE_INDEX = "CREATE INDEX IF NOT EXISTS idx_movies_release ON movies(release_timestamp);"
+WHERE users.username = %s;"""
+INSERT_WATCHED_MOVIE = "INSERT INTO watched (user_username, movie_id) VALUES (%s, %s)"
+SEARCH_MOVIES = "SELECT * FROM movies WHERE title LIKE %s;"
 
 
 def create_tables():
     with connection:
-        connection.execute(CREATE_MOVIES_TABLE)
-        connection.execute(CREATE_USERS_TABLE)
-        connection.execute(CREATE_WATCHED_TABLE)
-        connection.execute(CREATE_RELEASE_INDEX)
+        with connection.cursor() as cursor:
+            cursor.execute(CREATE_MOVIES_TABLE)
+            cursor.execute(CREATE_USERS_TABLE)
+            cursor.execute(CREATE_WATCHED_TABLE)
 
 def add_user(username):
     with connection:
-        connection.execute(INSERT_USER, (username,))
+        with connection.cursor() as cursor:
+            cursor.execute(INSERT_USER, (username,))
 
 
 def add_movie(title, release_timestamp):
     with connection:
-        connection.execute(INSERT_MOVIES, (title, release_timestamp))
+        with connection.cursor() as cursor:
+            cursor.execute(INSERT_MOVIES, (title, release_timestamp))
 
 def get_movies(upcoming=False):
     with connection:
-        Cursor = connection.cursor()
-        if upcoming:
-            today_timestamp = datetime.datetime.today().timestamp()
-            Cursor.execute(SELECT_UPCOMING_MOVIES, (today_timestamp,))
-        else:
-            Cursor.execute(SELECT_ALL_MOVIES)
-        return Cursor.fetchall
+        with connection.cursor() as cursor:
+            Cursor = connection.cursor()
+            if upcoming:
+                today_timestamp = datetime.datetime.today().timestamp()
+                Cursor.execute(SELECT_UPCOMING_MOVIES, (today_timestamp,))
+            else:
+                Cursor.execute(SELECT_ALL_MOVIES)
+            return Cursor.fetchall
 
 
 def search_movies(search_term):
     with connection:
-        Cursor = connection.cursor()
-        Cursor.execute(SEARCH_MOVIES, (f"%{search_term}%", ))
-        return Cursor.fetchall()
+        with connection.cursor() as cursor:
+            Cursor = connection.cursor()
+            Cursor.execute(SEARCH_MOVIES, (f"%{search_term}%", ))
+            return Cursor.fetchall()
 
 def watch_movie(username, movie_id):
     with connection:
-        connection.execute(INSERT_WATCHED_MOVIE, (username, movie_id))
+        with connection.cursor() as cursor:
+            cursor.execute(INSERT_WATCHED_MOVIE, (username, movie_id))
 
 def get_watched_movies(username):
     with connection:
-        Cursor = connection.cursor()
-        Cursor.execute(SELECT_WATCHED_MOVIES, (username, ))
-        return Cursor.fetchall()
+        with connection.cursor() as cursor:
+            Cursor = connection.cursor()
+            Cursor.execute(SELECT_WATCHED_MOVIES, (username, ))
+            return Cursor.fetchall()
