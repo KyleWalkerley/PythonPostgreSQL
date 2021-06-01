4c31b87020815cd08e1a22d3dca6563eb5cdc045
KyleWalkerley <78598168+KyleWalkerley@users.noreply.github.com>
2021-06-01 11:49:31 +0200

    Add files via upload

--- PythonPostgreSQL/Chapter9/graph_tests.py
+++ PythonPostgreSQL/Chapter9/graph_tests.py
@@ -1,9 +1,8 @@
 import matplotlib.pyplot as plt
 
-figure = plt.figure()
-axes = figure.add_subplot()
-axes.set_title("A test line graph")
-axes.set_xlabel("Numbers")
-axes.set_ylabel("Occurrences")
-axes.plot([1, 2, 3, 4], [3, 5, 9, 25])
+figure, (ax1, ax2) = plt.subplots(1, 2)
+
+ax1.plot([1, 2, 3, 4], [3, 5, 9, 25])
+ax2.plot([1, 2, 3, 4], [5, 7, 11, 17])
+
 plt.show()
